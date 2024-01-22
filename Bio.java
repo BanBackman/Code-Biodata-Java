72ee6efb11a05ad5c63b6054947abaea0e049451
BanBackman <148816204+BanBackman@users.noreply.github.com>
2024-01-23 01:35:16 +0700

    Add files via upload

---
+++ Bio.java
@@ -0,0 +1,17 @@
+public class Bio {
+
+    public static void main (String [] args){
+        String nama = "Rizky Pratama";
+        String alamat = "Jl. syailendra, kecamatan rawa sari";
+        int tahun_lahir = 2004;
+        int tahun_sekarang = 2022;
+        int umur = tahun_sekarang-tahun_lahir; 
+        
+        System.out.println("Nama :" + nama);
+        System.out.println("Alamat :" + alamat);
+        System.out.println("Tahun lahir :" + tahun_lahir);
+        System.out.println("Umur :" + umur);
+    }
+
+
+}
---
+++ biodata.java
@@ -0,0 +1,9 @@
+public class zakat {
+    public static void main(String[] args) {
+        int harta = 10000000;
+        System.out.println("Jumlah harta saya adalah "+harta);
+        System.out.println("Jumlah zakat saya adalah "+ (harta*25/1000));
+
+    }
+        
+}
